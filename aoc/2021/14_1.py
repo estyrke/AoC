cdc8ad4ab480abf0a7d9cf6642b550d2128ddbae
Emil Styrke <emil.styrke@axis.com>
2021-12-14 10:26:12 +0100

    Solved 2021/14

---
+++ aoc/2021/14_1.py
@@ -0,0 +1,152 @@
+from io import TextIOWrapper
+import math
+
+###
+# # ## --- Day 14: Extended Polymerization ---
+#
+# The incredible pressures at this depth are starting to put a strain on
+# your submarine. The submarine has
+# [polymerization](https://en.wikipedia.org/wiki/Polymerization)
+# equipment that would produce suitable materials to reinforce the
+# submarine, and the nearby volcanically-active caves should even have
+# the necessary input elements in sufficient quantities.
+#
+# The submarine manual contains instructions for finding the optimal
+# polymer formula; specifically, it offers a *polymer template* and a
+# list of *pair insertion* rules (your puzzle input). You just need to
+# work out what polymer would result after repeating the pair insertion
+# process a few times.
+#
+# For example:
+#
+#
+# ```
+# NNCB
+#
+# CH -> B
+# HH -> N
+# CB -> H
+# NH -> C
+# HB -> C
+# HC -> B
+# HN -> C
+# NN -> C
+# BH -> H
+# NC -> B
+# NB -> B
+# BN -> B
+# BB -> N
+# BC -> B
+# CC -> N
+# CN -> C
+#
+# ```
+#
+# The first line is the *polymer template* - this is the starting point
+# of the process.
+#
+# The following section defines the *pair insertion* rules. A rule like
+# `AB -> C` means that when elements `A` and `B` are immediately
+# adjacent, element `C` should be inserted between them. These
+# insertions all happen simultaneously.
+#
+# So, starting with the polymer template `NNCB`, the first step
+# simultaneously considers all three pairs:
+#
+# * The first pair (`NN`) matches the rule `NN -> C`, so element `*C*`
+# is inserted between the first `N` and the second `N`.
+# * The second pair (`NC`) matches the rule `NC -> B`, so element `*B*`
+# is inserted between the `N` and the `C`.
+# * The third pair (`CB`) matches the rule `CB -> H`, so element `*H*`
+# is inserted between the `C` and the `B`.
+#
+#
+# Note that these pairs overlap: the second element of one pair is the
+# first element of the next pair. Also, because all pairs are considered
+# simultaneously, inserted elements are not considered to be part of a
+# pair until the next step.
+#
+# After the first step of this process, the polymer becomes
+# `N*C*N*B*C*H*B`.
+#
+# Here are the results of a few steps using the above rules:
+#
+#
+# ```
+# Template:     NNCB
+# After step 1: NCNBCHB
+# After step 2: NBCCNBBBCBHCB
+# After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
+# After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
+#
+# ```
+#
+# This polymer grows quickly. After step 5, it has length 97; After step
+# 10, it has length 3073. After step 10, `B` occurs 1749 times, `C`
+# occurs 298 times, `H` occurs 191 times, and `N` occurs 865 times;
+# taking the quantity of the most common element (`B`, 1749) and
+# subtracting the quantity of the least common element (`H`, 161)
+# produces `1749 - 161 = *1588*`.
+#
+# Apply 10 steps of pair insertion to the polymer template and find the
+# most and least common elements in the result. *What do you get if you
+# take the quantity of the most common element and subtract the quantity
+# of the least common element?*
+###
+
+test_input = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"""
+
+test_output = 1588
+
+def do_insert(string, rules):
+    res = string[0]
+    for i in range(1, len(string)):
+        pair = string[i-1]+string[i]
+        if pair in rules:
+            res += rules[pair]
+        res += string[i]
+    return res
+
+def solve(inp: TextIOWrapper):
+    answer = None
+
+    templ, rules = inp.read().split("\n\n")
+
+    templ = templ.strip()
+    rules = [r.strip().split(" -> ") for r in rules.splitlines()]
+    rules = {r[0]: r[1] for r in rules}
+
+    print(templ)
+    print(rules)
+    # for line in inp.readlines():
+    # lines = [l for l in inp.readlines()]
+    res = templ
+    for i in range(10):
+        res = do_insert(res, rules)
+
+    res = list(res)
+    counts = []
+    for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        if res.count(c) > 0:
+            counts.append((c, res.count(c)))
+
+    counts.sort(key=lambda c: c[1])
+    print(counts)
+    return counts[-1][1] - counts[0][1]
