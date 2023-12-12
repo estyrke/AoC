983ac13f5575e692595960a04f1552d929cece29
Emil Styrke <emil.styrke@arboair.com>
2023-12-12 15:25:40 +0100

    2023/12: turns out the part1 solution was way faster when memoized, so scrapped the part2 variant...

--- aoc/2023/12.py
+++ aoc/2023/12.py
@@ -13,7 +13,8 @@ part1_test_input = """???.### 1,1,3
 part1_test_output = 21
 
 
-def split(record: list[str], groups: list[int], current_group=0) -> int:
+@lru_cache
+def split(record: str, groups: tuple[int], current_group=0) -> int:
     if len(record) == 0:
         if len(groups) == 0 or (len(groups) == 1 and current_group == groups[0]):
             return 1
@@ -37,34 +38,10 @@ def split(record: list[str], groups: list[int], current_group=0) -> int:
             return 0
         return split(record[1:], groups, current_group + 1)
     if record[0] == "?":
-        return split([".", *record[1:]], groups, current_group) + split(
-            ["#", *record[1:]], groups, current_group
+        return split("." + record[1:], groups, current_group) + split(
+            "#" + record[1:], groups, current_group
         )
     return 0
-
-
-@lru_cache
-def split2(record: str, groups: tuple[int]) -> int:
-    if len(groups) == 0:
-        if all([c in ["?", "."] for c in record]):
-            return 1
-        return 0
-
-    gl = groups[0]
-    res = 0
-    # print(f"{record} {groups}:")
-    for i in range(len(record) - gl + 1):
-        if all(c in "?." for c in record[0:i]) and all(
-            c in "?#" for c in record[i : i + gl]
-        ):
-            if i + gl == len(record):
-                if len(groups) == 1:
-                    # print(f"  Matched {gl} at {i} (final)")
-                    res += 1
-            elif record[i + gl] in "?.":
-                # print(f"  Matched {gl} at {i}")
-                res += split2(record[i + gl + 1 :], groups[1:])
-    return res
 
 
 def part1(inp: TextIOWrapper):
@@ -72,12 +49,9 @@ def part1(inp: TextIOWrapper):
 
     for line in inp.readlines():
         record, groups = line.strip().split()
-        record = list(record)
-        groups = [int(g) for g in groups.split(",")]
+        groups = tuple([int(g) for g in groups.split(",")])
 
-        split2.cache_clear()
-        answer += (s := split(record, groups))
-        # print(s)
+        answer += split(record, groups)
 
     return answer
 
@@ -90,14 +64,13 @@ part2_test_output = 525152
 def part2(inp: TextIOWrapper):
     answer = 0
 
-    for i, line in enumerate(inp.readlines()):
+    for line in inp.readlines():
         record, groups = line.strip().split()
         record = "?".join([record, record, record, record, record])
         groups = tuple(int(g) for g in groups.split(","))
         groups = (*groups, *groups, *groups, *groups, *groups)
 
-        answer += (s := split2(record, groups))
-        print(i + 1, s)
+        answer += split(record, groups)
     return answer
 
 
